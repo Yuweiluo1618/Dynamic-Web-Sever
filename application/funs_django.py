629357e5ce93eb5bbd5dd252fd09b3b4ebbcdcc5
Yuweiluo1618 <yuwei.luo@alumni.ubc.ca>
2021-01-16 23:17:51 -0800

    update Django url

---
+++ application/funs_django.py
@@ -0,0 +1,9 @@
+import time
+def index():
+    return "This is Index"
+
+def center():
+    return "This is Center"
+
+def gettime():
+    return f"This is Time {time.ctime()}"
---
+++ application/urls_django.py
@@ -0,0 +1,6 @@
+from application import funs
+route_dict={
+    '/index.py': funs.index,
+    '/center.py': funs.center,
+    '/gettime.py': funs.gettime
+}
